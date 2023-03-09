b1d27b65c62ee432e6d15c5dd08fd3075ee78b53
Juan Diego <jdalvarezlugo@gmail.com>
2023-03-09 18:28:27 -0500

    finish day three

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const tenthButton = document.getElementById('button10');
 const twelfthButton = document.getElementById('button12');
 const thirtiethButton = document.getElementById('button13');
 const fourtiethButton = document.getElementById('button14');
+const fifthiethButton = document.getElementById('button15');
 const sixteenButton = document.getElementById('button16');
 const seventeenButton = document.getElementById('button17');
 const eighteenButton = document.getElementById('button18');
@@ -18,11 +19,20 @@ const twentyonwhButton = document.getElementById('button21');
 const twentytwoButton = document.getElementById('button22');
 const twentythreeButton = document.getElementById('button23');
 const twentyfourButton = document.getElementById('button24');
+const twentyfiveButton = document.getElementById('button25');
+const oneButton = document.getElementById('button01');
+const twoButton = document.getElementById('button02');
 const hiddendiv1 = document.getElementById('hidden1');
 const hiddendiv2 = document.getElementById('hidden2');
+const hiddendiv3 = document.getElementById('hidden3');
+const hiddendiv4 = document.getElementById('hidden4');
+const user = document.getElementById('user');
+const resultsOfMinecraft = [];
+const resultsOfDemonSlayer = [];
 hiddendiv1.style.display = "none"
 hiddendiv2.style.display = "none"
-
+hiddendiv3.style.display = "none"
+hiddendiv4.style.display = "none"
 firstButton.addEventListener('click', () => {
     secondButton.style.border = "none";
     firstButton.style.border = "5px solid black"
@@ -39,89 +49,128 @@ fourthButton.addEventListener('click', () => {
     fourthButton.style.border = "5px solid black"
     fifthButton.style.border = "none"
     sixthButton.style.border = "none"
+    resultsOfMinecraft[0] = "Filo III"
 });
 fifthButton.addEventListener('click', () => {
     fifthButton.style.border = "5px solid black";
     fourthButton.style.border = "none"
     sixthButton.style.border = "none"
+    resultsOfMinecraft[0] = "Filo IV"
 });
 sixthButton.addEventListener('click', () => {
     sixthButton.style.border = "5px solid black"
     fourthButton.style.border = "none"
     fifthButton.style.border = "none"
+    resultsOfMinecraft[0] = "Filo V"
 });
+
 eighthButton.addEventListener('click', () => {
     eighthButton.style.border = "5px solid black"
     ninthButton.style.border = "none"
     tenthButton.style.border = "none"
+    resultsOfMinecraft[1] = "Skyblock"
 });
 ninthButton.addEventListener('click', () => {
     ninthButton.style.border = "5px solid black"
     eighthButton.style.border = "none"
     tenthButton.style.border = "none"
+    resultsOfMinecraft[1] = "Creativo"
 });
 tenthButton.addEventListener('click', () => {
     tenthButton.style.border = "5px solid black"
     ninthButton.style.border = "none"
     eighthButton.style.border = "none"
+    resultsOfMinecraft[1] = "Survival"
 });
+
 twelfthButton.addEventListener('click', () => {
     twelfthButton.style.border = "5px solid black"
     thirtiethButton.style.border = "none"
     fourtiethButton.style.border = "none"
+    resultsOfMinecraft[2] = "Hycraft"
 });
 thirtiethButton.addEventListener('click', () => {
     thirtiethButton.style.border = "5px solid black"
     twelfthButton.style.border = "none"
     fourtiethButton.style.border = "none"
+    resultsOfMinecraft[2] = "Hypixel"
 });
 fourtiethButton.addEventListener('click', () => {
     fourtiethButton.style.border = "5px solid black"
     twelfthButton.style.border = "none"
     thirtiethButton.style.border = "none"
-});
+    resultsOfMinecraft[2] = "UniversoCraft"
+})
+
+
+
 sixteenButton.addEventListener('click', () => {
     sixteenButton.style.border = "5px solid black"
     seventeenButton.style.border = "none"
     eighteenButton.style.border = "none"
+    resultsOfDemonSlayer[0] = "Tercera Luna Creciente"
 });
 seventeenButton.addEventListener('click', () => {
     seventeenButton.style.border = "5px solid black"
     sixteenButton.style.border = "none"
     eighteenButton.style.border = "none"
+    resultsOfDemonSlayer[0] = "Quinta Luna Menguante"
 });
 eighteenButton.addEventListener('click', () => {
     eighteenButton.style.border = "5px solid black"
     sixteenButton.style.border = "none"
     seventeenButton.style.border = "none"
+    resultsOfDemonSlayer[0] = "Primera Luna Menguante"
 });
 nineteenButton.addEventListener('click', () => {
     nineteenButton.style.border = "5px solid black"
     twentiethButton.style.border = "none"
     twentyonwhButton.style.border = "none"
+    resultsOfDemonSlayer[1] = "Muzan Kibutzuji"
 });
 twentiethButton.addEventListener('click', () => {
     twentiethButton.style.border = "5px solid black"
     nineteenButton.style.border = "none"
     twentyonwhButton.style.border = "none"
+    resultsOfDemonSlayer[1] = "Yushiro Tamano"
 });
 twentyonwhButton.addEventListener('click', () => {
     twentyonwhButton.style.border = "5px solid black"
     twentiethButton.style.border = "none"
     nineteenButton.style.border = "none"
+    resultsOfDemonSlayer[1] = "Tamayo Kunado"
 });
 twentytwoButton.addEventListener('click', () => {
     twentytwoButton.style.border = "5px solid black"
     twentythreeButton.style.border = "none"
     twentyfourButton.style.border = "none"
+    resultsOfDemonSlayer[2] = "Urokodaki Sakonji"
 });
 twentythreeButton.addEventListener('click', () => {
     twentythreeButton.style.border = "5px solid black"
     twentytwoButton.style.border = "none"
     twentyfourButton.style.border = "none"
+    resultsOfDemonSlayer[2] = "Jigoro Kuwajima"
 });
 twentyfourButton.addEventListener('click', () => {
     twentyfourButton.style.border = "5px solid black"
     twentytwoButton.style.border = "none"
     twentythreeButton.style.border = "none"
+    resultsOfDemonSlayer[2] = "Yoriichi Tsugikuni"
 });
+fifthiethButton.addEventListener('click', () => {
+    document.getElementById('resultsOfMinecraft').innerHTML = resultsOfMinecraft
+    document.getElementById('resultsOfMinecraft').style.display = "block"
+})
+twentyfiveButton.addEventListener('click', () => {
+    document.getElementById('resultsOfDemonSlayer').innerHTML = resultsOfDemonSlayer
+    document.getElementById('resultsOfDemonSlayer').style.display = "block"
+})
+oneButton.addEventListener('click', () => {
+    hiddendiv3.style.display = "block"
+    hiddendiv4.style.display = "none"
+})
+twoButton.addEventListener('click', () => {
+    hiddendiv3.style.display = "none"
+    hiddendiv4.style.display = "block"
+})
