ad8f02026bc47bac86671fbeed8f3c2964a71d8d
Juan Diego <jdalvarezlugo@gmail.com>
2023-04-02 15:21:17 -0500

    finish day 5 && add full height to every question

--- script.js
+++ script.js
@@ -19,22 +19,15 @@ const twentyonwhButton = document.getElementById('button21');
 const twentytwoButton = document.getElementById('button22');
 const twentythreeButton = document.getElementById('button23');
 const twentyfourButton = document.getElementById('button24');
-const twentyfiveButton = document.getElementById('button25');
 const oneButton = document.getElementById('button01');
-const twoButton = document.getElementById('button02');
 const hiddendiv1 = document.getElementById('hidden1');
 const hiddendiv2 = document.getElementById('hidden2');
-const hiddendiv3 = document.getElementById('hidden3');
-const hiddendiv4 = document.getElementById('hidden4');
 const corrrectAnswersOfDemonSlayer = ["Quinta Luna Menguante", "Muzan kibutsuji", "Urokodaki Sakonji"]
 const corrrectAnswersOfMinecraft = ["Filo V", "Skyblock", "Hypixel"]
-
 const resultsOfMinecraft = [];
 const resultsOfDemonSlayer = [];
 hiddendiv1.style.display = "none"
 hiddendiv2.style.display = "none"
-hiddendiv3.style.display = "none"
-hiddendiv4.style.display = "none"
 const user = document.getElementById('user');
 let userName = '';
 
@@ -109,18 +102,45 @@ twelfthButton.addEventListener('click', () => {
     thirtiethButton.style.border = "none"
     fourtiethButton.style.border = "none"
     resultsOfMinecraft[2] = "Hycraft"
+    document.getElementById('resultsOfMinecraft').innerHTML = resultsOfMinecraft
+    document.getElementById('resultsOfMinecraft').style.display = "block"
+    if (resultsOfMinecraft.length === 3) {
+        document.getElementById('important-h2').style.display = "block"
+        document.getElementById('correctAnswersOfMinecraft').style.display = "block"
+        document.getElementById('correctAnswersOfMinecraft').innerHTML = corrrectAnswersOfMinecraft
+        document.getElementById('answer3').style.display = "block"
+        document.getElementById('answer1').style.display = "block"
+    }
 });
 thirtiethButton.addEventListener('click', () => {
     thirtiethButton.style.border = "5px solid black"
     twelfthButton.style.border = "none"
     fourtiethButton.style.border = "none"
     resultsOfMinecraft[2] = "Hypixel"
+    document.getElementById('resultsOfMinecraft').innerHTML = resultsOfMinecraft
+    document.getElementById('resultsOfMinecraft').style.display = "block"
+    if (resultsOfMinecraft.length === 3) {
+        document.getElementById('important-h2').style.display = "block"
+        document.getElementById('correctAnswersOfMinecraft').style.display = "block"
+        document.getElementById('correctAnswersOfMinecraft').innerHTML = corrrectAnswersOfMinecraft
+        document.getElementById('answer3').style.display = "block"
+        document.getElementById('answer1').style.display = "block"
+    }
 });
 fourtiethButton.addEventListener('click', () => {
     fourtiethButton.style.border = "5px solid black"
     twelfthButton.style.border = "none"
     thirtiethButton.style.border = "none"
     resultsOfMinecraft[2] = "UniversoCraft"
+    document.getElementById('resultsOfMinecraft').innerHTML = resultsOfMinecraft
+    document.getElementById('resultsOfMinecraft').style.display = "block"
+    if (resultsOfMinecraft.length === 3) {
+        document.getElementById('important-h2').style.display = "block"
+        document.getElementById('correctAnswersOfMinecraft').style.display = "block"
+        document.getElementById('correctAnswersOfMinecraft').innerHTML = corrrectAnswersOfMinecraft
+        document.getElementById('answer3').style.display = "block"
+        document.getElementById('answer1').style.display = "block"
+    }
 })
 
 
@@ -166,48 +186,40 @@ twentytwoButton.addEventListener('click', () => {
     twentythreeButton.style.border = "none"
     twentyfourButton.style.border = "none"
     resultsOfDemonSlayer[2] = "Urokodaki Sakonji"
+    document.getElementById('resultsOfDemonSlayer').innerHTML = resultsOfDemonSlayer
+    document.getElementById('resultsOfDemonSlayer').style.display = "block"
+    if (resultsOfDemonSlayer.length === 3) {
+        document.getElementById('answer2').style.display = "block"
+        document.getElementById('correctAnswersOfDemonSlayer').style.display = "block"
+        document.getElementById('correctAnswersOfDemonSlayer').innerHTML = corrrectAnswersOfDemonSlayer
+        document.getElementById('answer2').style.display = "block"
+    }
 });
 twentythreeButton.addEventListener('click', () => {
     twentythreeButton.style.border = "5px solid black"
     twentytwoButton.style.border = "none"
     twentyfourButton.style.border = "none"
     resultsOfDemonSlayer[2] = "Jigoro Kuwajima"
+    document.getElementById('resultsOfDemonSlayer').innerHTML = resultsOfDemonSlayer
+    document.getElementById('resultsOfDemonSlayer').style.display = "block"
+    if (resultsOfDemonSlayer.length === 3) {
+        document.getElementById('answer2').style.display = "block"
+        document.getElementById('correctAnswersOfDemonSlayer').style.display = "block"
+        document.getElementById('correctAnswersOfDemonSlayer').innerHTML = corrrectAnswersOfDemonSlayer
+        document.getElementById('answer4').style.display = "block"
+    }
 });
 twentyfourButton.addEventListener('click', () => {
     twentyfourButton.style.border = "5px solid black"
     twentytwoButton.style.border = "none"
     twentythreeButton.style.border = "none"
     resultsOfDemonSlayer[2] = "Yoriichi Tsugikuni"
-});
-fifthiethButton.addEventListener('click', () => {
-    document.getElementById('resultsOfMinecraft').innerHTML = resultsOfMinecraft
-    document.getElementById('resultsOfMinecraft').style.display = "block"
-})
-twentyfiveButton.addEventListener('click', () => {
     document.getElementById('resultsOfDemonSlayer').innerHTML = resultsOfDemonSlayer
     document.getElementById('resultsOfDemonSlayer').style.display = "block"
-})
-oneButton.addEventListener('click', () => {
-    hiddendiv3.style.display = "block"
-    hiddendiv4.style.display = "none"
-})
-twoButton.addEventListener('click', () => {
-    hiddendiv3.style.display = "none"
-    hiddendiv4.style.display = "block"
-})
-twentyfiveButton.addEventListener('click', () => {
     if (resultsOfDemonSlayer.length === 3) {
         document.getElementById('answer2').style.display = "block"
         document.getElementById('correctAnswersOfDemonSlayer').style.display = "block"
         document.getElementById('correctAnswersOfDemonSlayer').innerHTML = corrrectAnswersOfDemonSlayer
+        document.getElementById('answer4').style.display = "block"
     }
-    document.getElementById('answer3').style.display = "block"
-})
-fifthiethButton.addEventListener('click', () => {
-    if (resultsOfMinecraft.length === 3) {
-        document.getElementById('answer1').style.display = "block"
-        document.getElementById('correctAsnwersOfMinecraft').style.display = "block"
-        document.getElementById('correctAsnwersOfMinecraft').innerHTML = corrrectAnswersOfMinecraft
-    }
-    document.getElementById('answer4').style.display = "block"
-})
+});
